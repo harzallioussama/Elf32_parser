b4557d7584f821992f956168f0244ee1be89f443
harous <oussamaharzalli6@gmail.com>
2025-03-15 05:01:12 +0100

    Added section headers parsing

--- elf_parser.c
+++ elf_parser.c
@@ -6,6 +6,7 @@
 #include <unistd.h>
 #include <string.h>
 #include <stdbool.h>
+#include <assert.h>
 
 bool elf_check_file(Elf32_Ehdr *elf_header)
 {
@@ -109,6 +110,157 @@ void print_segment_flags(uint32_t flags)
     printf("%s%9s", f, "");
 }
 
+const char *get_section_type_name(uint32_t type)
+{
+    switch (type)
+    {
+    case SHT_NULL:
+        return "NULL (Unused)";
+    case SHT_PROGBITS:
+        return "PROGBITS (Program Data)";
+    case SHT_SYMTAB:
+        return "SYMTAB (Symbol Table)";
+    case SHT_STRTAB:
+        return "STRTAB (String Table)";
+    case SHT_RELA:
+        return "RELA (Relocations with Addends)";
+    case SHT_HASH:
+        return "HASH (Symbol Hash Table)";
+    case SHT_DYNAMIC:
+        return "DYNAMIC (Dynamic Linking Info)";
+    case SHT_NOTE:
+        return "NOTE (Notes)";
+    case SHT_NOBITS:
+        return "NOBITS (BSS)";
+    case SHT_REL:
+        return "REL (Relocations without Addends)";
+    case SHT_SHLIB:
+        return "SHLIB (Reserved)";
+    case SHT_DYNSYM:
+        return "DYNSYM (Dynamic Symbol Table)";
+    case SHT_INIT_ARRAY:
+        return "INIT_ARRAY (Constructors)";
+    case SHT_FINI_ARRAY:
+        return "FINI_ARRAY (Destructors)";
+    case SHT_PREINIT_ARRAY:
+        return "PREINIT_ARRAY (Pre-constructors)";
+    case SHT_GROUP:
+        return "GROUP (Section Group)";
+    case SHT_SYMTAB_SHNDX:
+        return "SYMTAB_SHNDX (Extended Section Indices)";
+    case SHT_NUM:
+        return "NUM (Number of Defined Types)";
+    case SHT_LOOS:
+        return "LOOS (Start OS-specific)";
+    case SHT_GNU_ATTRIBUTES:
+        return "GNU_ATTRIBUTES (Object Attributes)";
+    case SHT_GNU_HASH:
+        return "GNU_HASH (GNU-Style Hash Table)";
+    case SHT_GNU_LIBLIST:
+        return "GNU_LIBLIST (Prelink Library List)";
+    case SHT_CHECKSUM:
+        return "CHECKSUM (Checksum for DSO Content)";
+    case SHT_LOSUNW:
+        return "LOSUNW (Sun-Specific Low Bound)";
+    case SHT_SUNW_COMDAT:
+        return "SUNW_COMDAT";
+    case SHT_SUNW_syminfo:
+        return "SUNW_syminfo";
+    case SHT_GNU_verdef:
+        return "GNU_verdef (Version Definition Section)";
+    case SHT_GNU_verneed:
+        return "GNU_verneed (Version Needs Section)";
+    case SHT_GNU_versym:
+        return "GNU_versym (Version Symbol Table)";
+    case SHT_LOPROC:
+        return "LOPROC (Start of Processor-Specific)";
+    case SHT_HIPROC:
+        return "HIPROC (End of Processor-Specific)";
+    case SHT_LOUSER:
+        return "LOUSER (Start of Application-Specific)";
+    case SHT_HIUSER:
+        return "HIUSER (End of Application-Specific)";
+    default:
+        return "UNKNOWN";
+    }
+}
+
+void get_section_flags(unsigned int flags, char *buffer, size_t size) {
+    buffer[0] = '\0';
+
+    if (flags & SHF_WRITE)            strncat(buffer, "W", size - strlen(buffer) - 1);
+    if (flags & SHF_ALLOC)            strncat(buffer, "A", size - strlen(buffer) - 1);
+    if (flags & SHF_EXECINSTR)        strncat(buffer, "X", size - strlen(buffer) - 1);
+    if (flags & SHF_MERGE)            strncat(buffer, "M", size - strlen(buffer) - 1);
+    if (flags & SHF_STRINGS)          strncat(buffer, "S", size - strlen(buffer) - 1);
+    if (flags & SHF_INFO_LINK)        strncat(buffer, "I", size - strlen(buffer) - 1);
+    if (flags & SHF_LINK_ORDER)       strncat(buffer, "L", size - strlen(buffer) - 1);
+    if (flags & SHF_OS_NONCONFORMING) strncat(buffer, "O", size - strlen(buffer) - 1);
+}
+
+void parse_elf32_shdr(int fd, Elf32_Ehdr *elf_header)
+{
+    uint32_t shder_entry_size = elf_header->e_shentsize;
+    uint32_t shder_nums_entry = elf_header->e_shnum;
+    uint32_t shder_offset = elf_header->e_shoff;
+    uint32_t shder_size = shder_entry_size * shder_nums_entry;
+    char *elf_section_header_table = (char *)malloc(shder_size);
+    off_t offset = lseek(fd, shder_offset, SEEK_SET);
+    if (offset == -1)
+    {
+        printf("Couldn't lseek file, errno %d\n", errno);
+        exit(-1);
+    }
+    ssize_t rd = read(fd, (void *)elf_section_header_table, shder_size);
+    if (rd < 0)
+    {
+        printf("Couldn't read file, errno %d\n", errno);
+        exit(-1);
+    }
+    assert(rd == (ssize_t)shder_size);
+    Elf32_Shdr elf_shder;
+    uint32_t shstrtab_index = elf_header->e_shstrndx;
+    memcpy((char *)&elf_shder, elf_section_header_table + (shstrtab_index * shder_entry_size), shder_entry_size);
+    uint32_t shstrtab_offset = elf_shder.sh_offset;
+    uint32_t shstrtab_size = elf_shder.sh_size;
+    char *elf_shstrtab_table = (char *)malloc(shstrtab_size);
+    offset = lseek(fd, shstrtab_offset, SEEK_SET);
+    if (offset == -1)
+    {
+        printf("Couldn't lseek file, errno %d\n", errno);
+        exit(-1);
+    }
+    rd = read(fd, (void *)elf_shstrtab_table, (size_t)shstrtab_size);
+    if (rd < 0)
+    {
+        printf("Couldn't read file, errno %d\n", errno);
+        exit(-1);
+    }
+    assert(rd == shstrtab_size);
+    puts("Section Headers:");
+    printf("  [Nr]   %-20s   %-40s %6s  %6s    %6s %6s    %s   %2s  %6s  %4s\n",
+        "Name", "Type", "Addr", "Off", "Size", "ES", "Flg", "Lk", "Inf", "Al");
+        for (uint32_t i = 0; i < shder_nums_entry; ++i)
+    {
+        memcpy((char *)&elf_shder, elf_section_header_table + (i * shder_entry_size), shder_entry_size);
+        char flag_str[9];
+        get_section_flags(elf_shder.sh_flags, flag_str, sizeof(flag_str));
+        printf("  [%2d] ", i);
+        printf("  %-20s ", (char *)(elf_shstrtab_table + elf_shder.sh_name));
+        printf("  %-40s ", get_section_type_name(elf_shder.sh_type));
+        printf("  0x%.4x ", elf_shder.sh_addr);
+        printf("  0x%.4x ", elf_shder.sh_offset);
+        printf("  0x%.4x ", elf_shder.sh_size);
+        printf("  0x%.2x ", elf_shder.sh_entsize);
+        printf("  %-2s ", flag_str);
+        printf("  0x%.2x ", elf_shder.sh_link);
+        printf("  0x%.2x ", elf_shder.sh_info);
+        printf("  0x%.2x \n", elf_shder.sh_addralign);
+    }
+    free(elf_section_header_table);
+    free(elf_shstrtab_table);
+}
+
 void parse_elf32_phdr(int fd, Elf32_Ehdr *elf_header)
 {
     uint32_t phder_entry_size = elf_header->e_phentsize;
@@ -122,20 +274,20 @@ void parse_elf32_phdr(int fd, Elf32_Ehdr *elf_header)
         printf("Couldn't lseek file, errno %d\n", errno);
         exit(-1);
     }
-    ssize_t rd = read(fd, (void *)elf_prog_header_table, phder_size);
+    ssize_t rd = read(fd, (void *)elf_prog_header_table, (size_t)phder_size);
     if (rd < 0)
     {
         printf("Couldn't read file, errno %d\n", errno);
         exit(-1);
     }
-    //int ffd = open("./file", O_WRONLY);
+    assert(rd == phder_size);
     puts("Program Headers:");
 
     printf("  p_type%12sp_offset    p_vaddr     p_paddr     p_filesz    p_memsz     p_flags     p_align\n", "");
     for (uint32_t i = 0; i < phder_nums_entry; ++i)
     {
         Elf32_Phdr elf_prog_header;
-        memcpy((char *)&elf_prog_header, (char *)(elf_prog_header_table + (i * phder_entry_size)), sizeof(Elf32_Phdr));
+        memcpy((char *)&elf_prog_header, (elf_prog_header_table + (i * phder_entry_size)), sizeof(Elf32_Phdr));
         print_segment_type(elf_prog_header.p_type);
         printf("0x%.8x  ", elf_prog_header.p_offset);
         printf("0x%.8x  ", elf_prog_header.p_vaddr);
@@ -146,6 +298,7 @@ void parse_elf32_phdr(int fd, Elf32_Ehdr *elf_header)
         printf("0x%.8x  ", elf_prog_header.p_align);
         puts("");
     }
+    free(elf_prog_header_table);
 }
 
 int main(int argc, char **argv)
@@ -176,4 +329,6 @@ int main(int argc, char **argv)
     parse_elf32_header(elf_header);
     printf("\n\n");
     parse_elf32_phdr(fd, elf_header);
+    printf("\n\n");
+    parse_elf32_shdr(fd, elf_header);
 }
